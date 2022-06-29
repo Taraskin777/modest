57ae9567e02730e2a45f800fef315b085c372fef
Taras Noga <taraskin2012@gmail.com>
2022-06-29 20:44:01 +0300

    add modal

--- app.js
+++ app.js
@@ -1,13 +1,37 @@
-
-
 let mobileMenu = document.querySelector(".nav-mobile-menu");
 let mainMenu = document.querySelector(".navigation");
 
 mobileMenu.addEventListener("click", function () {
-   mobileMenu.classList.toggle("active-menu");
-   if (mobileMenu.classList.contains("active-menu")) {
-      mainMenu.classList.add("active-menu");
+  mobileMenu.classList.toggle("active-menu");
+  if (mobileMenu.classList.contains("active-menu")) {
+    mainMenu.classList.add("active-menu");
+  } else {
+    mainMenu.classList.remove("active-menu");
+  }
+});
+
+// modal
+
+const modal = document.querySelector(".modal");
+const closemodal = document.querySelector(".modal__close");
+const sendbutton = document.querySelector(".send-but");
+
+function showModal() {
+  sendbutton.addEventListener("click", () => {
+    if (modal.classList.contains("hide")) {
+      modal.classList.add("show");
+      modal.classList.remove("hide");
     } else {
-      mainMenu.classList.remove("active-menu");
+      modal.classList.remove("hide");
+      modal.classList.add("show");
     }
-}) 
+  });
+  closemodal.addEventListener("click", () => {
+    modal.classList.add("hide");
+  });
+}
+
+showModal();
+
+
+
